89bbc722e8ef8b3f0601b37e28b5574e51d3e3ed
Alexssandro017 <20223tn017@utez.edu.mx>
2025-06-29 16:07:41 -0600

    chore: initial project structure with base folders and files

---
+++ src/test/java/mx/edu/utez/procli/ProcliApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.procli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
